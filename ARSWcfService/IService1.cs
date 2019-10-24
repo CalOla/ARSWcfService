e0316524171eac1b7c74016cb89475a280a72d4a
CalOla <ckolaoye@aggies.ncat.edu>
2019-10-23 21:17:42 -0700

    Add project files.

---
+++ ARSWcfService/IService1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ARSWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string AddUser(InsertUser user);
+
+        [OperationContract]
+        string AddFlight(InsertFlight flight);
+
+        [OperationContract]
+        DataTable GetSignInCredentials(string username, string password);
+
+        [OperationContract]
+        DataTable GetAllFlights();
+
+        [OperationContract]
+        DataTable GetMyFlights(string username);
+
+        [OperationContract]
+        string AddSeat(InsertSeat seat);
+
+        [OperationContract]
+        DataTable GetSelectedSeats(string flightName);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class InsertUser
+    {
+        string firstName = string.Empty;
+        string lastName = string.Empty;
+        string userName = string.Empty;
+        string email = string.Empty;
+        string userPassword = string.Empty;
+        string gender = string.Empty;
+        int isAdmin = 0;
+
+
+                
+
+        [DataMember]
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        [DataMember]
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        [DataMember]
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        [DataMember]
+        public string UserPassword
+        {
+            get { return userPassword; }
+            set { userPassword = value; }
+        }
+
+        [DataMember]
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        [DataMember]
+        public int IsAdmin
+        {
+            get { return isAdmin; }
+            set { isAdmin = value; }
+        }
+
+    }
+
+
+    [DataContract]
+    public class InsertFlight
+    {
+        string flightNumber = string.Empty;
+        string departureAirport = string.Empty;
+        string arrivalAirport = string.Empty;
+        string departureTime = string.Empty;
+        string arrivalTime = string.Empty;
+        string departureDate = string.Empty;
+        string firstClassPrice = string.Empty;
+        string economyClassPrice = string.Empty;
+        int seatingCapacity = 0;
+
+
+        [DataMember]
+        public string FlightNumber
+        {
+            get { return flightNumber; }
+            set { flightNumber = value; }
+        }
+
+        [DataMember]
+        public string DepartureAirport
+        {
+            get { return departureAirport; }
+            set { departureAirport = value; }
+        }
+
+        [DataMember]
+        public string ArrivalAirport
+        {
+            get { return arrivalAirport; }
+            set { arrivalAirport = value; }
+        }
+
+        [DataMember]
+        public string DepartureTime
+        {
+            get { return departureTime; }
+            set { departureTime = value; }
+        }
+
+        [DataMember]
+        public string ArrivalTime
+        {
+            get { return arrivalTime; }
+            set { arrivalTime = value; }
+        }
+
+        [DataMember]
+        public string DepartureDate
+        {
+            get { return departureDate; }
+            set { departureDate = value; }
+        }
+
+        [DataMember]
+        public string FirstClassPrice
+        {
+            get { return firstClassPrice; }
+            set { firstClassPrice = value; }
+        }
+
+        [DataMember]
+        public string EconomyClassPrice
+        {
+            get { return economyClassPrice; }
+            set { economyClassPrice = value; }
+        }
+
+        [DataMember]
+        public int SeatingCapacity
+        {
+            get { return seatingCapacity; }
+            set { seatingCapacity = value; }
+        }
+    }
+
+    [DataContract]
+    public class InsertSeat
+    {
+        string flightNumber = string.Empty;
+        string seatNumber = string.Empty;
+        string username = string.Empty;
+
+        
+        [DataMember]
+        public string FlightNumber
+        {
+            get { return flightNumber; }
+            set { flightNumber = value; }
+        }
+
+        [DataMember]
+        public string SeatNumber
+        {
+            get { return seatNumber; }
+            set { seatNumber = value; }
+        }
+
+        [DataMember]
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+    }
+
+
+    }
---
+++ ARSWcfService/Service1.svc.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ARSWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string AddUser(InsertUser user)
+        {
+            string message;
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ARSDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Insert into UserTable (Firstname, Lastname, Email, UserPassword, IsAdmin, Gender, UserName) values(@FirstName, @LastName, @Email, @UserPassword, @IsAdmin, @Gender, @UserName)", con);
+                cmd.Parameters.AddWithValue("@FirstName", user.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", user.LastName);
+                cmd.Parameters.AddWithValue("@UserName", user.UserName);
+                cmd.Parameters.AddWithValue("@Email", user.Email);
+                cmd.Parameters.AddWithValue("@UserPassword", user.UserPassword);
+                cmd.Parameters.AddWithValue("@Gender", user.Gender);
+                cmd.Parameters.AddWithValue("@IsAdmin", user.IsAdmin);
+
+                int response = cmd.ExecuteNonQuery();
+                if (response == 1)
+                {
+                    message = "Successfully Inserted";
+                }
+                else
+                {
+                    message = "Failed to insert";
+                }
+                con.Close();
+                return message;
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                return e.Message;
+            }
+
+        }
+
+        public string AddFlight(InsertFlight flight)
+        {
+            string message;
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ARSDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                string command = "Insert into FlightTable (FlightNumber, DepartureAirport, ArrivalAirport, DepartureTime, ArrivalTime, DepartureDate, FirstClassPrice, EconomyClassPrice, SeatingCapacity) " +
+                    "values(@FlightNumber, @DepartureAirport, @ArrivalAirport, @DepartureTime, @ArrivalTime, @DepartureDate, @FirstClassPrice, @EconomyClassPrice, @SeatingCapacity)";
+                SqlCommand cmd = new SqlCommand(command, con);
+                cmd.Parameters.AddWithValue("@FlightNumber", flight.FlightNumber);
+                cmd.Parameters.AddWithValue("@DepartureAirport", flight.DepartureAirport);
+                cmd.Parameters.AddWithValue("@ArrivalAirport", flight.ArrivalAirport);
+                cmd.Parameters.AddWithValue("@DepartureTime", flight.DepartureTime);
+                cmd.Parameters.AddWithValue("@ArrivalTime", flight.ArrivalTime);
+                cmd.Parameters.AddWithValue("@DepartureDate", flight.DepartureDate);
+                cmd.Parameters.AddWithValue("@FirstClassPrice", flight.FirstClassPrice);
+                cmd.Parameters.AddWithValue("@EconomyClassPrice", flight.EconomyClassPrice);
+                cmd.Parameters.AddWithValue("@SeatingCapacity", flight.SeatingCapacity);
+
+                int response = cmd.ExecuteNonQuery();
+                if (response == 1)
+                {
+                    message = "Successfully Inserted";
+                }
+                else
+                {
+                    message = "Failed to insert";
+                }
+                con.Close();
+                return message;
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                return e.Message;
+            }
+        }
+
+        public DataTable GetSignInCredentials(string username, string password)
+        {
+            DataTable dataTable = new DataTable("UserTable");
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ARSDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                string command = "SELECT UserPassword, UserName, IsAdmin FROM UserTable WHERE UserPassword = '" + password +"' AND UserName = '"+ username + "'";
+                SqlCommand cmd = new SqlCommand(command, con);
+                cmd.Parameters.AddWithValue("@UserName", username);
+                cmd.Parameters.AddWithValue("@UserPassword", password);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(dataTable);
+
+                con.Close();
+                return dataTable;
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                return dataTable;
+            }
+
+        }
+
+        public DataTable GetAllFlights()
+        {
+            DataTable dataTable = new DataTable("FlightTable");
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ARSDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                string command = "SELECT * FROM FlightTable";
+                SqlCommand cmd = new SqlCommand(command, con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(dataTable);
+
+                con.Close();
+                return dataTable;
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                return dataTable;
+            }
+        }
+
+        public DataTable GetMyFlights(string username)
+        {
+            DataTable dataTable = new DataTable("SeatTable");
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ARSDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                string command = "SELECT * FROM SeatTable JOIN FlightTable ON SeatTable.FlightNumber = FlightTable.FlightNumber WHERE SeatTable.Username = '" + username + "'";
+                SqlCommand cmd = new SqlCommand(command, con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(dataTable);
+
+                con.Close();
+                return dataTable;
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                return dataTable;
+            }
+        }
+
+        public DataTable GetSelectedSeats(string flightNumber)
+        {
+            DataTable dataTable = new DataTable("SeatTable");
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ARSDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                string command = "SELECT SeatNumber FROM SeatTable WHERE FlightNumber = '" + flightNumber + "'";
+                SqlCommand cmd = new SqlCommand(command, con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(dataTable);
+
+                con.Close();
+                return dataTable;
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                return dataTable;
+            }
+        }
+
+        public string AddSeat(InsertSeat seat)
+        {
+            string message;
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ARSDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Insert into SeatTable ( Username, SeatNumber, FlightNumber) values(@Username, @SeatNumber, @FlightNumber)", con);
+                cmd.Parameters.AddWithValue("@SeatNumber", seat.SeatNumber);
+                cmd.Parameters.AddWithValue("@FlightNumber", seat.FlightNumber);
+                cmd.Parameters.AddWithValue("@UserName", seat.Username);
+                
+
+                int response = cmd.ExecuteNonQuery();
+                if (response == 1)
+                {
+                    message = "Successfully Inserted";
+                }
+                else
+                {
+                    message = "Failed to insert";
+                }
+                con.Close();
+                return message;
+            }
+            catch (Exception e)
+            {
+                con.Close();
+                return e.Message;
+            }
+        }
+
+    }
+}
